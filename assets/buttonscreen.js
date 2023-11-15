afb63925c46f029f2f7b4234ae26cab73be2949b
Brahim Chaouki <b.chaouki@sio.local>
2023-11-15 10:59:37 +0100

    fichiers commit

---
+++ assets/buttonscreen.js
@@ -0,0 +1,59 @@
+
+	// Pop-up 
+    $(document).on('click', '#icon', function(event) {
+        swal({
+            title: 'A.S.A du Vercors<a href="http://www.asaduvercors.fr/" style="font-size: large;" target="_blank"> lien</a>',
+            text: 'Site internet crée sur mesure en HTML/CSS & JAVASCRIPT lors de mon stage chez BiGroup',
+            imageUrl: 'assets/images/asaduvercors.png',
+            imageWidth: 2000,
+            imageHeight: 2000,
+            imageAlt: 'A.S.A du Vercors',
+            animation: true
+        })
+    });
+    $(document).on('click', '#icon2', function(event) {
+        swal({
+            title: 'SIInfo<a href="https://siinfo07.wordpress.com/" style="font-size: large;" target="_blank"> lien</a>',
+            text: 'Site internet crée en Drag & Drop sur Wordpress lors de mon stage chez BiGroup',
+            imageUrl: 'assets/images/siinfo.png',
+            imageWidth: 2000,
+            imageHeight: 2000,
+            imageAlt: 'SIInfo',
+            animation: true
+        })
+    });
+    $(document).on('click', '#icon3', function(event) {
+        swal({
+            title: 'Boissier Electricité SAS<a href="https://www.electricite-boissier.fr/" style="font-size: large;" target="_blank"> lien</a>',
+            text: 'Site internet crée en Drag & Drop sur SimplySite lors de mon stage chez BiGroup',
+            imageUrl: 'assets/images/boissier.png',
+            imageWidth: 2000,
+            imageHeight: 2000,
+            imageAlt: 'Boissier',
+            animation: true
+        })
+    });
+    $(document).on('click', '#icon4', function(event) {
+        swal({
+            title: 'Monplusbeauvoyage<a href="https://monplusbeauvoyage1.wordpress.com/" style="font-size: large;" target="_blank"> lien</a>',
+            text: "Site internet crée en Drag & Drop sur Wordpress lors d'un projet scolaire à l'Institut supérieur de Saint-Denis",
+            imageUrl: 'assets/images/monplusbeauvoyage.png',
+            imageWidth: 2000,
+            imageHeight: 2000,
+            imageAlt: 'Monplusbeauvoyage',
+            animation: true
+        })
+    });
+    $(document).on('click', '#icon5', function(event) {
+        swal({
+            title: ' fterdtretee!',
+            text: "Alert with a custom image.",
+            imageUrl: 'https://image.shutterstock.com/z/stock-vector--exclamation-mark-exclamation-mark-hazard-warning-symbol-flat-design-style-vector-eps-444778462.jpg',
+            imageWidth: 2000,
+            imageHeight: 2000,
+            imageAlt: 'Custom image',
+            animation: true
+        })
+    });
+    
+
