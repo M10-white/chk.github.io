acaa812b9462acd9f89b6241e5e51eed788af0c8
Brahim Chaouki <bchaouki00@gmail.com>
2024-02-02 15:51:29 +0100

    Commit 020224

--- assets/buttonscreen.js
+++ assets/buttonscreen.js
@@ -2,7 +2,7 @@
 	// Pop-up 
     $(document).on('click', '#icon', function(event) {
         swal({
-            title: 'A.S.A du Vercors<a href="http://www.asaduvercors.fr/" style="font-size: large;" target="_blank">LIEN</a>',
+            title: 'A.S.A du Vercors<a href="http://www.asaduvercors.fr/" style="font-size: large;" target="_blank"> LIEN</a>',
             text: 'Site internet crée sur mesure en HTML/CSS & JAVASCRIPT lors de mon stage chez BiGroup',
             imageUrl: 'assets/images/asaduvercors.png',
             imageWidth: 2000,
@@ -13,7 +13,7 @@
     });
     $(document).on('click', '#icon2', function(event) {
         swal({
-            title: 'SIInfo<a href="https://siinfo07.wordpress.com/" style="font-size: large;" target="_blank">LIEN</a>',
+            title: 'SIInfo<a href="https://siinfo07.wordpress.com/" style="font-size: large;" target="_blank"> LIEN</a>',
             text: 'Site internet crée en Drag & Drop sur Wordpress lors de mon stage chez BiGroup',
             imageUrl: 'assets/images/siinfo.png',
             imageWidth: 2000,
@@ -24,7 +24,7 @@
     });
     $(document).on('click', '#icon3', function(event) {
         swal({
-            title: 'Boissier Electricité SAS<a href="https://www.electricite-boissier.fr/" style="font-size: large;" target="_blank">LIEN</a>',
+            title: 'Boissier Electricité SAS<a href="https://www.electricite-boissier.fr/" style="font-size: large;" target="_blank"> LIEN</a>',
             text: 'Site internet crée en Drag & Drop sur SimplySite lors de mon stage chez BiGroup',
             imageUrl: 'assets/images/boissier.png',
             imageWidth: 2000,
@@ -35,7 +35,7 @@
     });
     $(document).on('click', '#icon4', function(event) {
         swal({
-            title: 'Monplusbeauvoyage <a href="https://monplusbeauvoyage1.wordpress.com/" style="font-size: large;" target="_blank">LIEN</a>',
+            title: 'Monplusbeauvoyage <a href="https://monplusbeauvoyage1.wordpress.com/" style="font-size: large;" target="_blank"> LIEN</a>',
             text: "Site internet crée en Drag & Drop sur Wordpress lors d'un projet scolaire à l'Institut supérieur de Saint-Denis",
             imageUrl: 'assets/images/monplusbeauvoyage.png',
             imageWidth: 2000,
@@ -46,9 +46,9 @@
     });
     $(document).on('click', '#icon5', function(event) {
         swal({
-            title: 'ECO QUEST <a href="https://geniesfou.alwaysdata.net/" style="font-size: large;" target="_blank">LIEN</a>',
+            title: 'ECO QUEST <a href="https://geniesfou.alwaysdata.net/" style="font-size: large;" target="_blank"> LIEN</a>',
             text: "Site internet crée sur mesure en HTML/CSS/PHP/MySQL & JAVASCRIPT lors de la Nuit de L'info 2023 à l'Institut supérieur de Saint-Denis",
-            imageUrl: 'https://image.shutterstock.com/z/stock-vector--exclamation-mark-exclamation-mark-hazard-warning-symbol-flat-design-style-vector-eps-444778462.jpg',
+            imageUrl: 'assets/images/ecoQuest.png',
             imageWidth: 2000,
             imageHeight: 2000,
             imageAlt: 'Custom image',
